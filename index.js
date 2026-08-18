8af1954980a91b539ae4af7bfc61d7633388cd15
phatpt1 <26410260@ms.uit.edu.vn>
2026-08-18 10:12:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+export default {
+  async fetch(request) {
+    const targetUrl = "https://ehou-tinhoc.streamlit.app";
+    const url = new URL(request.url);
+    const target = new URL(targetUrl);
+
+    url.hostname = target.hostname;
+    url.protocol = target.protocol;
+
+    const modifiedRequest = new Request(url.toString(), {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: "follow"
+    });
+
+    return fetch(modifiedRequest);
+  }
+};
